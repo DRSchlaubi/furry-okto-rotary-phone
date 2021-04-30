abcb149ca9b632bf47a3542f35b4597b761a60fc
schlaubi <michael@rittmeister.in>
2021-04-30 23:41:57 +0200

    Add gradle for sample

---
+++ analyze-client-sample/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "dev.schlaubi.forp"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://schlaubi.jfrog.io/artifactory/forp")
+}
+
+dependencies {
+    implementation(project(":forp-analyze:forp-analyze-client"))
+    implementation("org.slf4j", "slf4j-simple", "1.7.30")
+}
